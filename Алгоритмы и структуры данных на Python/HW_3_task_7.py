80837ad79a5f960256f68d23d68804eb5d3739a8
TataMoskovkina <TataMoskovkina@users.noreply.github.com>
2019-01-22 01:21:57 +0300

    Update HW_3_task_7.py

--- Алгоритмы и структуры данных на Python/HW_3_task_7.py
+++ Алгоритмы и структуры данных на Python/HW_3_task_7.py
@@ -8,4 +8,16 @@ Created on Fri Jan 18 01:22:55 2019
 Они могут быть как равны между собой (оба являться минимальными), 
 так и различаться.
 """
+from random import randint
 
+n = int(input("Введите длинну массива: "))
+nums = [randint(0, 20) for i in range(n)]
+print(nums) #проверка
+
+min_1 = min(nums)
+nums.pop(nums.index(min(nums)))
+print(nums) #проверка
+
+min_2 = min(nums)
+
+print(f'Первые два наименьших числа в массиве равны: {min_1} и {min_2}')
