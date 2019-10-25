828f3fb305b333f6f7fe2fca7a36fd21eea00603
TataMoskovkina <44415672+TataMoskovkina@users.noreply.github.com>
2019-10-26 02:43:50 +0300

    Update superjob.py

--- Data mining/superjob.py
+++ Data mining/superjob.py
@@ -1,18 +1,23 @@
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
-import re
-import random
-import time
+from pymongo import MongoClient
+from pprint import pprint
+import unicodedata
+
+client = MongoClient('localhost',27017)
+vacancy_db = client.vacancy_db
+superjob = vacancy_db.superjob
 
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 YaBrowser/19.9.0.1343 Yowser/2.5 Safari/537.36'
 
-base_url = ('https://www.superjob.ru/')
+base_url = ('https://www.superjob.ru')
 url = ('https://www.superjob.ru/vacancy/search/?keywords=python&geo%5Bc%5D%5B0%5D=1')
 html = requests.get(url, headers={'User-Agent': USER_AGENT}).text
 parsed_html = bs(html, 'lxml')
 
-vacancys = pd.DataFrame({'vacancy_name': [], 'requirements': [], 'salary_min': [], 'salary_max':[], 'currency': [], 'town':[], 'base_url': []})
+vacancys = pd.DataFrame({'vacancy_name': [], 'requirements': [], 'salary_min': [], 'salary_max':[], 'currency': [],
+                         'town':[], 'vacancy_url': [], 'base_url': []})
 vacancy_list = []
 
 def get_next_page(parsed_html):
@@ -39,28 +44,38 @@ def get_vacancy(vacancy):
     try:
         salarys = vacancy.find('span', {'class': 'f-test-text-company-item-salary'}).text
         if '—' in salarys:
-            salary = salarys.split('—')
+            salary = unicodedata.normalize("NFKD", salarys).split('—')
             sal = str(salary[1]).split(' ')
-            salary_min = salary[0].replace(r'\xa', ' ')
-            salary_max = sal[0].replace(r'\xa', ' ')
+            salary_min = int(salary[0].replace(' ', ''))
+            salary_max = int(sal[1]+sal[2])
             currency = 'руб'
         elif 'от' in salarys:
             salary = salarys.split(' ')
-            salary_min = salary[0].replace(r'\xa0', ' ')
-            salary_max = ''
+            sal = unicodedata.normalize("NFKD", salary[0]).split(' ')
+            salary_min = int(sal[1]+sal[2])
+            salary_max = None
             currency = 'руб'
         elif 'По договорённости' in salarys:
             salary_min = 'По договорённости'
-            salary_max = ''
-            currency = ''
+            salary_max = None
+            currency = None
+        else:
+            salary = unicodedata.normalize("NFKD", salarys).split(' ')
+            salary_min = int(salary[0]+salary[1])
+            salary_max = None
+            currency = 'руб'
     except AttributeError:
-        salary_min = ''
-        salary_max = ''
-        currency = ''
+        salary_min = None
+        salary_max = None
+        currency = None
+
     town = vacancy.find('span', {'class': '_3mfro _9fXTd _2JVkc _3e53o _3Ll36'}).next.next.next.text
     requirements = vacancy.find('div', {'class':'_2kyiZ _2XXYS _2cxK3'}).text
+    vacancy_url = base_url + vacancy.find('div', attrs={'class': '_3mfro CuJz5 PlM3e _2JVkc _3LJqf'}).findParent()['href']
 
-    result = [vacancy_name, requirements, salary_min, salary_max, currency, town, base_url]
+    result = {'vacancy_name': vacancy_name, 'requirements': requirements,
+              'salary_min': salary_min, 'salary_max': salary_max, 'currency': currency,
+              'town': town, 'vacancy_url': vacancy_url, 'base_url': base_url}
 
     return result
 
@@ -70,6 +85,14 @@ i = 0
 for vacancy in vacancy_list:
     result = get_vacancy(vacancy)
     vacancys.loc[i] = result
+    superjob.update_one({'vacancy_url': result['vacancy_url']},
+                        {'$set': result},
+                        upsert=True)
     i += 1
 
-vacancys.to_csv('superjob.csv', index=False)
+objects = superjob.find({'salary_min': {'$gte': 100000}}, {'vacancy_name', 'salary_min', 'vacancy_url'})
+for obj in objects:
+    pprint(obj)
+
+#vacancys.to_csv('superjob.csv', index=False)
+
