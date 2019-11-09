ccd45f56856348de2d60f185bbe0130eaed5c6e1
TataMoskovkina <44415672+TataMoskovkina@users.noreply.github.com>
2019-11-09 04:26:22 +0300

    Update hhru.py

--- Data mining/scrapy/jobparser/spiders/hhru.py
+++ Data mining/scrapy/jobparser/spiders/hhru.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from jobparser.items import JobparserItem
+from scrapy.loader import ItemLoader
 
 class HhruSpider(scrapy.Spider):
     name = 'hhru'
@@ -19,10 +20,17 @@ class HhruSpider(scrapy.Spider):
             yield response.follow(link, callback=self.vacansy_parse)
 
     def vacansy_parse(self, response):
-        name = response.css('div.vacancy-title h1.header::text').extract_first()
+        #name = response.css('div.vacancy-title h1.header::text').extract_first()
         url_vacancy = response.url
-        min_salary = response.css('meta[itemprop="minValue"]::attr(content)').extract_first()
-        max_salary = response.css('meta[itemprop="maxValue"]::attr(content)').extract_first()
-        source = 'hh.ru'
-        yield JobparserItem(name=name, url_vacancy=url_vacancy, min_salary=min_salary,
-                            max_salary=max_salary, source=source)
+        #min_salary = response.css('meta[itemprop="minValue"]::attr(content)').extract_first()
+        #max_salary = response.css('meta[itemprop="maxValue"]::attr(content)').extract_first()
+        #source = 'hh.ru'
+        #yield JobparserItem(name=name, url_vacancy=url_vacancy, min_salary=min_salary,
+        #                    max_salary=max_salary, source=source)
+
+        loader = ItemLoader(item=JobparserItem(), response=response)
+        loader.add_css('name', 'div.vacancy-title h1.header::text')
+        loader.add_value('url_vacancy', response.url)
+        loader.add_css('min_salary', 'meta[itemprop="minValue"]::attr(content)')
+        loader.add_css('max_salary', 'meta[itemprop="maxValue"]::attr(content)')
+        loader.add_value('source', 'hh.ru')
